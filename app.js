17d548b5b93287f91c05746807456add4005bcde
abejide001 <abejidefemi1@gmail.com>
2021-01-04 06:06:11 +0100

    commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express")
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.json({
+        message: "this is a test"
+    })
+})
+
+const port = process.env.PORT || 4000
+
+app.listen(port, () => {
+    console.log(`listening on ${port}`)
+})
