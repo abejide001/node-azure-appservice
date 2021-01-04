b388c1e3b79aaec89373aed169a7e6fea22023c0
abejide001 <abejidefemi1@gmail.com>
2021-01-04 12:25:31 +0100

    fix

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-const express = require("express")
+const express = require("express");
 
-const app = express()
+const app = express();
 
-app.get('/', (req, res) => {
-    res.json({
-        message: "this is a test"
-    })
-})
+app.get("/", (req, res) => {
+  res.json({
+    message: "this is a test",
+  });
+});
 
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
-    console.log(`listening on ${port}`)
-})
+  console.log(`listening on ${port}`);
+});
